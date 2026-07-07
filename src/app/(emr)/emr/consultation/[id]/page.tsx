5058ab45963ec851a3c43be543ba51891b2dd406
kidneycare40-lang <kidneycare40-lang@users.noreply.github.com>
2026-07-07 18:09:11 +0530

    Revert: restore max-w-5xl padding on consultation page

--- src/app/(emr)/emr/consultation/[id]/page.tsx
+++ src/app/(emr)/emr/consultation/[id]/page.tsx
@@ -1027,7 +1027,7 @@ export default function ConsultationPage() {
           />
 
           <div className="flex-1 overflow-y-auto pb-20">
-            <div className="p-4 space-y-4">
+            <div className="max-w-5xl mx-auto p-4 space-y-4">
               <ErrorBoundary>
                 <ReportUploadOCR
                   onApplyLabValues={handleApplyLabValues}
